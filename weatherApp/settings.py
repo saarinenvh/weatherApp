74267537f3418d4b15f9e02f2c49d36bdc500216
vhs <ville.h.saarinen@aalto.fi>
2018-03-12 12:52:38 +0200

    settings changes

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -84,7 +84,11 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
