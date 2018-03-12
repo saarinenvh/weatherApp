9101378fc53e4ed245ae7a75430c3b5826f4a92e
vhs <ville.h.saarinen@aalto.fi>
2018-03-12 12:10:00 +0200

    dir changes for heroku

--- weatherApp/weather/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from weather.models import WeatherInfo
-
-class addInfo(ModelForm):
-    class Meta:
-        model = WeatherInfo
-        fields = ['temperature', 'weather', 'comment']
