ef6aafca9af9a70b6e586d1901c62bfef6beabb9
vhs <ville.h.saarinen@aalto.fi>
2018-03-12 12:47:18 +0200

    m

--- weatherApp/settings.py
+++ weatherApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,13 +84,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
-
-
+DATABASES['default'] =  dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
